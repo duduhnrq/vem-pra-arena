392d82740dead08d88df7c2f4402664df332f325
Santos-dev25 <caua34584@gmail.com>
2026-05-16 00:21:01 -0300

    V spring

--- plataforma/src/main/java/com/vempraarena/plataforma/Application.java
+++ plataforma/src/main/java/com/vempraarena/plataforma/Application.java
@@ -2,6 +2,10 @@ package com.vempraarena.plataforma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +13,13 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-	
+
+    // Bean que injeta o limite de 100MB direto no Tomcat (Ignora configurações antigas)
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(100));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(100));
+        return factory.createMultipartConfig();
+    }
 }
