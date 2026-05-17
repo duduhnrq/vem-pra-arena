2f39f4bbb7adf4aedf710408d85fbdc2d06119a5
duduhnrq <dudu30rafa@gmail.com>
2026-05-16 22:31:21 -0300

    Update Spring Boot Requirements

--- plataforma/src/main/java/com/vempraarena/plataforma/Application.java
+++ plataforma/src/main/java/com/vempraarena/plataforma/Application.java
@@ -2,24 +2,11 @@ package com.vempraarena.plataforma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import jakarta.servlet.MultipartConfigElement;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    // Bean que injeta o limite de 100MB direto no Tomcat (Ignora configurações antigas)
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        factory.setMaxFileSize(DataSize.ofMegabytes(100));
-        factory.setMaxRequestSize(DataSize.ofMegabytes(100));
-        return factory.createMultipartConfig();
     }
 }
